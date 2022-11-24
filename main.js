6e2599ec68e535c976a19ebf407f0ccbbea94a24
Jhonny <gonzodzen@mail.ru>
2022-11-25 00:21:10 +0300

    No Loops 2 - You only need one from codewars

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-function warnTheSheep(queue) {
-    let indexWolf = queue.indexOf('wolf')
-    return indexWolf===queue.length-1?"Pls go away and stop eating my sheep":`Oi! Sheep number ${(queue.length-1) - indexWolf}! You are about to be eaten by a wolf!`
- }
-  console.log(warnTheSheep(["sheep", "wolf", "sheep", "sheep", "sheep", "sheep", "sheep"]))
+function check(a,x){
+    return a.some(el=>el===x)
+  }
+  
