d847555a009d7e0e4d815a6158b5c90ee380bb2d
Jhonny <gonzodzen@mail.ru>
2022-11-24 23:30:53 +0300

    A wolf in sheep's clothing from codewars

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-function vowelIndices(word){
-    let arr = []
-    word.split('').map((el,i)=>{
-        el==='a' || el==='e' || el==='i' || el==='o' || el==='u' || el==='y' ||
-        el==='A' || el==='E' || el==='I' || el==='O' || el==='U' || el==='Y' ? el[arr.push(i+1)]:null
-    })
-    return arr
-  }
-  console.log(vowelIndices("super"))
+function warnTheSheep(queue) {
+    let indexWolf = queue.indexOf('wolf')
+    return indexWolf===queue.length-1?"Pls go away and stop eating my sheep":`Oi! Sheep number ${(queue.length-1) - indexWolf}! You are about to be eaten by a wolf!`
+ }
+  console.log(warnTheSheep(["sheep", "wolf", "sheep", "sheep", "sheep", "sheep", "sheep"]))
