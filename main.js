2d4d99dd95212b845bb8592688bd02a4208ef671
Jhonny <gonzodzen@mail.ru>
2022-11-24 22:56:55 +0300

    Find the vowels fromcodewars

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-function lowercaseCount(str){
-    if(!str) 0
-     let newStr = str.match(/[a-z]/g)
-     return !newStr?0:newStr.length
-}
-console.log(lowercaseCount(""))
+function vowelIndices(word){
+    let arr = []
+    word.split('').map((el,i)=>{
+        el==='a' || el==='e' || el==='i' || el==='o' || el==='u' || el==='y' ||
+        el==='A' || el==='E' || el==='I' || el==='O' || el==='U' || el==='Y' ? el[arr.push(i+1)]:null
+    })
+    return arr
+  }
+  console.log(vowelIndices("super"))
