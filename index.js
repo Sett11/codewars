158985f97c923830a0b0bc169987ab08ce9d3d43
Sett11 <109938931+Sett11@users.noreply.github.com>
2022-12-02 11:19:50 +0300

    Update index.js
    Chunky Monkey from freecodecamp

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-function mutation(arr) {
-  let result = null
-  let newArr = [arr[0].toLowerCase(), arr[1].toLowerCase()]
-  let ar1 = newArr[0].split('')
-  let ar2 = newArr[1].split('')
-  for(let i = 0; i<ar2.length; i++){
-    if(ar1.indexOf(ar2[i])>=0){
-      result = true
-    }
-    else if(ar1.indexOf(ar2[i])<0){
-      result = false
-      break
-    }
+function chunkArrayInGroups(arr, size) {
+  let newArr = []
+  let count = 0
+  while(count<arr.length){
+    newArr.push(arr.slice(count, count += size))
   }
-  return result
+  return newArr
 }
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
