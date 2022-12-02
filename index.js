34e93863c7a8243f83a4234b7672d773ef248022
Sett11 <109938931+Sett11@users.noreply.github.com>
2022-12-02 10:50:47 +0300

    Update index.js
    Mutations from freecodecamp

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function getIndexToIns(arr, num) {
-   arr.push(num)
-  let newArr = arr.sort((a,b)=>a-b)
-  return newArr.indexOf(num)
+function mutation(arr) {
+  let result = null
+  let newArr = [arr[0].toLowerCase(), arr[1].toLowerCase()]
+  let ar1 = newArr[0].split('')
+  let ar2 = newArr[1].split('')
+  for(let i = 0; i<ar2.length; i++){
+    if(ar1.indexOf(ar2[i])>=0){
+      result = true
+    }
+    else if(ar1.indexOf(ar2[i])<0){
+      result = false
+      break
+    }
+  }
+  return result
 }
-
-getIndexToIns([40, 60], 50);
