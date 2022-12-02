121a9b1bcc2cd2a38970ab49b179df8da76c019a
Sett11 <109938931+Sett11@users.noreply.github.com>
2022-12-02 14:17:24 +0300

    Update index.js
    Reset an Inherited Constructor Property from freecodecamp

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function Animal() { }
-
-Animal.prototype = {
-  constructor: Animal,
-  eat: function() {
-    console.log("nom nom nom");
-  }
-};
-
+function Bird() { }
 function Dog() { }
-// Only change code below this line
+
+Bird.prototype = Object.create(Animal.prototype);
 Dog.prototype = Object.create(Animal.prototype);
 
+// Only change code below this line
+
+
+Bird.prototype.constructor = Bird;
+let duck = new Bird();
+Dog.prototype.constructor = Dog;
 let beagle = new Dog();
