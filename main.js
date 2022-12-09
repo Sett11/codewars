aae46f89fd6e8de4dd056a948d764e804c3e1627
Sett11 <109938931+Sett11@users.noreply.github.com>
2022-12-09 15:32:29 +0300

    Indexed capitalization

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-function multiples(m, n){
-  let arr = []
-    let newN = n
-  for(let i = 0; i < m; i++){
-    arr.push(n)
-    n+=newN
-  }
-    return arr
+function capitalize(s,arr){
+  let newStr = s.split('')
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > newStr.length){
+            let w = arr.indexOf(arr[i])
+            arr.splice(w,1)
+        } else{
+            newStr[arr[i]] = newStr[arr[i]].toUpperCase()
+        }
+    }
+    return newStr.join('')
 }
