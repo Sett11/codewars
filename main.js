6babd5220029eb103533377583d4e7cef0ba38bd
Sett11 <109938931+Sett11@users.noreply.github.com>
2022-12-09 17:38:29 +0300

    Dashatize it

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-function grabDoll(dolls){
-  var bag=[];
-  for(let i = 0; i < dolls.length; i++){
-      if(dolls[i] !== "Hello Kitty" && dolls[i] !== "Barbie doll"){
-           continue
-      }
-      bag.push(dolls[i])
-      if(bag.length >= 3){
-          break
-      } 
-  }
-  return bag;
+function dashatize(num) {
+    if(num < 0){
+        num = Math.abs(num)
+    }
+    if(Number.isNaN(Math.abs(num))){
+        return 'NaN'
+    }
+    const v = (''+num).split('').map(Number).map((el) => {
+        if(el % 2 !== 0){
+            el = '-' + el + '-'
+            return el
+        }else{
+            return el
+        }
+    }).join('').split('')
+    for(let i = 0; i < v.length; i++){
+        if(v[i] === '-' && v[i + 1] === '-'){
+            v[i] = '&'
+        }
+    }
+    if(v[0] === '-'){
+        v[0] = '&'
+    }
+    if(v[v.length - 1] === '-'){
+        v[v.length - 1] = '&'
+    }
+    return v.filter(el => el !== '&').join('')
 }
