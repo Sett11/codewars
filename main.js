34fea47b1bff6c4df967272db0dcc5e5fb053802
Sett11 <109938931+Sett11@users.noreply.github.com>
2022-12-09 16:36:52 +0300

    String transformer

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-function solve(s){
-    const arr = []
-s.split('').map((el) => {
-     if(el === 'a' || el === 'u' || el === 'i' || el === 'e' || el === 'o'){
-         return el
-     } else{
-         el = 0
-         return el
-     }
- }).join('').split(/0/g).map((el) => arr.push(el.split('').length))
-    const res = arr.sort((a,b) => b - a)
-    return res[0]
+function stringTransformer(str) {
+  return str.split(' ').map((el) => {
+    return [...el].map((el) => {
+          if(el === el.toUpperCase()){
+          el = el.toLowerCase()
+          return el
+      } else{
+          el = el.toUpperCase()
+          return el
+      }
+      }).join('')
+  }).reverse().join(' ')
 }
