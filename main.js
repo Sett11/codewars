54f2f7e361e492deb6d9230aad954b7e34e2a63e
Sett11 <109938931+Sett11@users.noreply.github.com>
2022-12-16 11:01:33 +0300

    Linked Lists - Push & BuildOneTwoThree

--- main.js
+++ main.js
@@ -1,28 +1,19 @@
-function dashatize(num) {
-    if(num < 0){
-        num = Math.abs(num)
-    }
-    if(Number.isNaN(Math.abs(num))){
-        return 'NaN'
-    }
-    const v = (''+num).split('').map(Number).map((el) => {
-        if(el % 2 !== 0){
-            el = '-' + el + '-'
-            return el
-        }else{
-            return el
-        }
-    }).join('').split('')
-    for(let i = 0; i < v.length; i++){
-        if(v[i] === '-' && v[i + 1] === '-'){
-            v[i] = '&'
-        }
-    }
-    if(v[0] === '-'){
-        v[0] = '&'
-    }
-    if(v[v.length - 1] === '-'){
-        v[v.length - 1] = '&'
-    }
-    return v.filter(el => el !== '&').join('')
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function push(head, data) {
+  let newHead = null
+  newHead = new Node(data)
+  newHead.next = head
+  return newHead
+}
+
+function buildOneTwoThree() {
+  let tail = null
+  tail = push(tail, 3)
+  tail = push(tail, 2)
+  tail = push(tail, 1)
+  return tail
 }
