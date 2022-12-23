dc6b1947bb889a38e1587f03de184d5ccfeeeb42
Sett11 <109938931+Sett11@users.noreply.github.com>
2022-12-23 16:15:54 +0300

    filterEvenLengthWords

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-function counterEffect(h) {
- return h.split('').map(el => el = [Number(el)]).map(el => {
-     let counter = 0
-     if(el[0] !== counter){
-         while(counter <= el[0]){
-             el.push(counter)
-             counter++
-         }
-     }
-     return el
- }).map((el) => {
-     if(el[0] === 0 && el.length === 1){
-         return el
-     } else{
-         el.shift()
-         return el
-     }
- })
+function filterEvenLengthWords(words) {
+  return words.filter(el => el.length % 2 === 0)
 }
