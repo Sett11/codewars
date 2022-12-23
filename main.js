22e7a6444861ebd79a1985328be693aeed2f6e76
Sett11 <109938931+Sett11@users.noreply.github.com>
2022-12-23 15:28:50 +0300

    Substring fun

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-function tailSwap(arr) {
-   const strLinc = arr.map(el => el.split(':'))
-    const u = strLinc[0][1]
-    const o = strLinc[1][1]
-   strLinc[0][1] =   o
-    strLinc[1][1] =  u
-  return strLinc.map(el => el.join(':'))
+function nthChar(words){
+ return words.map((el, i) => {
+   el = el.charAt(i)
+   return el
+ }).join('')
 }
