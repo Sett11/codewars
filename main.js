ff27d510e80c036e06357a1c80e1baa1d706e33c
Sett11 <109938931+Sett11@users.noreply.github.com>
2022-12-23 16:06:04 +0300

    Hit Count

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-function borrow(s){
-  return s.replace(/\W/g, '').toLowerCase()
+function counterEffect(h) {
+ return h.split('').map(el => el = [Number(el)]).map(el => {
+     let counter = 0
+     if(el[0] !== counter){
+         while(counter <= el[0]){
+             el.push(counter)
+             counter++
+         }
+     }
+     return el
+ }).map((el) => {
+     if(el[0] === 0 && el.length === 1){
+         return el
+     } else{
+         el.shift()
+         return el
+     }
+ })
 }
