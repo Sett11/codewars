6c685c8376e0395d57056c7bd0ab9077a4c1975d
Sett11 <109938931+Sett11@users.noreply.github.com>
2022-12-23 14:03:59 +0300

    Sum of all arguments

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-function caffeineBuzz(n){
-  return n % 3 === 0 && n % 4 === 0 && n % 2 === 0 ? "CoffeeScript" :
-  n % 3 === 0 && n % 4 === 0 && n % 2 !== 0? "Coffee" :
-  n % 3 === 0 && n % 2 === 0 ? "JavaScript" :
-  n % 3 === 0 && n % 2 !== 0 ? 'Java' :
-  "mocha_missing!"
+function sum() {
+  const arr = [...arguments]
+  return arr.reduce((acc, curr) => acc+curr,0)
 }
