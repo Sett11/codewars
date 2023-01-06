94561c775f2d5115b6da4d3596c6c0fad7771e20
Sett11 <109938931+Sett11@users.noreply.github.com>
2023-01-06 16:06:57 +0300

    Array2Binary addition

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-function pluck(objs, name) {
-return objs.map(el=>el[name])
+function arr2bin(arr){
+return arr.every(el=>Number.isInteger(el))&&
+  (arr.reduce((acc,curr)=>acc+curr,0)).toString(2)
 }
