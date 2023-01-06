268f01a8c2f345300d9c3f25c81a7c801d74a30b
Sett11 <109938931+Sett11@users.noreply.github.com>
2023-01-06 16:46:18 +0300

    Composing squared strings

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-function arr2bin(arr){
-return arr.every(el=>Number.isInteger(el))&&
-  (arr.reduce((acc,curr)=>acc+curr,0)).toString(2)
+function compose(s1, s2) {
+   s1 = s1.split`\n`, s2 = s2.split`\n`, str = ''
+    for(let i = 0, j = s2.length-1; i < s1.length, j >= 0; i++, j--){
+        str += s1[i].slice(0,i+1) + s2[j].slice(0,j+1) + '\n'
+    }
+    return str.slice(0, str.length-1)
 }
