077009062ab651a95805906a2419df2f0985f2ae
Sett11 <109938931+Sett11@users.noreply.github.com>
2023-03-03 17:29:26 +0300

    Prime reduction

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-function prime(n) {
+function solve(a, b) {
+  console.log(a,b)
   const F=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
-  let arr=[],c=2
-  while(c<=n){if(F(c))arr.push(c);c++}
-  return arr
+  let c=0
+  for(let i=a;i<=b;i++){
+    if(F(i)){
+      let tmp=[...i+''],val=true
+      while(val){
+        tmp=tmp.map(e=>e**2).reduce((a,c)=>a+c)
+        if([...tmp+''].length===1&&tmp===1){
+          c+=1
+          val=false
+        }
+        if([...tmp+''].length===1&&tmp!==1&&tmp!==7&&tmp!==8&&tmp!==9&&tmp!==5&&tmp!==6){
+          val=false
+        }
+        else{
+          tmp=[...tmp+'']
+        }
+      }
+    }
+  }return c
 }
