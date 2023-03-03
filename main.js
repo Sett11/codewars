c141bb27d1b847ddf1eccf5ebad5c7e1412770bf
Sett11 <109938931+Sett11@users.noreply.github.com>
2023-03-03 16:06:27 +0300

    (Ready for) Prime Time

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-function compose(s1, s2) {
-   s1 = s1.split`\n`, s2 = s2.split`\n`, str = ''
-    for(let i = 0, j = s2.length-1; i < s1.length, j >= 0; i++, j--){
-        str += s1[i].slice(0,i+1) + s2[j].slice(0,j+1) + '\n'
-    }
-    return str.slice(0, str.length-1)
+function prime(n) {
+  const F=x=>{if(x<2)return false;if(x===2)return true;for(let i=2;i<Math.sqrt(x)+1;i++){if(x%i===0)return false}return true}
+  let arr=[],c=2
+  while(c<=n){if(F(c))arr.push(c);c++}
+  return arr
 }
