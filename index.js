12f68ceb3ffd54d230b77ef0c41f617d15de7bb3
Sett11 <109938931+Sett11@users.noreply.github.com>
2023-03-24 15:46:17 +0300

    Split string by multiple delimiters

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function spongeMeme(s) {
- return [...s].map((e,i)=>i%2===0?e.toUpperCase():e.toLowerCase()).join``
+function multipleSplit(s,d=[]){
+ if(d.length===1&&d[0]===' ')return s.split` `.filter(e=>e)
+ if(!d.length)return [s].filter(e=>e)
+  s=[...s]
+  for(let i=0;i<s.length;i++)if(d.includes(s[i]))s[i]=' '
+  return s.join``.split` `.filter(e=>e)
 }
+
