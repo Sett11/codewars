295b1645edac7d57d3966adea3bda1b6567349cc
Sett11 <109938931+Sett11@users.noreply.github.com>
2023-03-24 17:44:57 +0300

    Adding remainders to a list

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-function cleanItUp(s){
-  if(!s)return ''
-  s=s.replace(/[^a-zA-Z]/g,'')
-  const a=[]
-  for(let i=0;i<s.length;i+=5)a.push(s.slice(i,i+5))
-  return a.filter(e=>e.length===5).map(e=>e.charAt(0).toUpperCase()+e.slice(1).toLowerCase()).join` `
-}
+solve=(n,d)=>n.map(e=>e+(e%d))
