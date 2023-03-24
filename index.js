cecd0a1f2bc2bd54f073712143d99042420d46d4
Sett11 <109938931+Sett11@users.noreply.github.com>
2023-03-24 15:49:41 +0300

    Square(n) Sum

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-function multipleSplit(s,d=[]){
- if(d.length===1&&d[0]===' ')return s.split` `.filter(e=>e)
- if(!d.length)return [s].filter(e=>e)
-  s=[...s]
-  for(let i=0;i<s.length;i++)if(d.includes(s[i]))s[i]=' '
-  return s.join``.split` `.filter(e=>e)
-}
+squareSum=n=>n.reduce((a,c)=>a+ c**2,0)
 
