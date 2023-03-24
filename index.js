154d18996ab39f6b95dc1d5f80569e8157887e86
Sett11 <109938931+Sett11@users.noreply.github.com>
2023-03-24 16:04:17 +0300

    Nothing special

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-function squareIt(n){
-  n=[...n+'']
-  if(Number.isInteger(Math.sqrt(n.length))){
-     let a=[],c=Math.sqrt(n.length)
-    for(let i=0;i<n.length;i+=c)a.push(n.slice(i,i+c).join``)
-    return a.join`\n`
-  }
-	return 'Not a perfect square!';
-}
+nothingSpecial=s=>typeof s!=='string'?'Not a string!':s.replace(/[^A-Za-z0-9\t ]/g,'')
 
