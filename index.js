1c8c1b3b83b91456f8e6f7397aede153c9ac3fb1
Sett11 <109938931+Sett11@users.noreply.github.com>
2023-03-24 15:58:03 +0300

    Perfect squares, perfect fun

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-squareSum=n=>n.reduce((a,c)=>a+ c**2,0)
+function squareIt(n){
+  n=[...n+'']
+  if(Number.isInteger(Math.sqrt(n.length))){
+     let a=[],c=Math.sqrt(n.length)
+    for(let i=0;i<n.length;i+=c)a.push(n.slice(i,i+c).join``)
+    return a.join`\n`
+  }
+	return 'Not a perfect square!';
+}
 
